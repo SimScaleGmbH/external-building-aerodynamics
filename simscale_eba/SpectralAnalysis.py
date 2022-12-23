94e05bad6685e2183f86951be706af3a9d656a8f
Guillermo Giraldo <ggiraldo@simscale.com>
2022-12-23 11:08:31 -0500

    Initial refactoring.

--- simscale_eba/SpectralAnalysis.py
+++ simscale_eba/SpectralAnalysis.py
@@ -4,16 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-component = "UMag_AIJ_Case_C"
-probe = "65"
-path = pathlib.Path.cwd() / "{}.csv".format(component)
-data = pd.read_csv(path, index_col="Time (s)")
 
-data = data["{}".format(probe)]
-
-
-class frequency_analysis():
-
+class frequency_analysis:
     def __init__(self):
         self.data = None
 
@@ -26,7 +18,8 @@ class frequency_analysis():
     def set_data(self, data):
         self.data = data
 
-    def get_integral_length_scale(self):
+    def compute_integral_length_scale(self):
+        # Legacy code
         data = self.data
         signal = data.to_numpy()
         U_mean = np.mean(signal)
@@ -44,7 +37,7 @@ class frequency_analysis():
         aFreq = fs * (np.arange(1, n) / n)
         energy = power / fs
         ef = np.mean(energy[2:100])
-        hatx = (ef * U_mean) / (4 * U_stddev ** 2)
+        hatx = (ef * U_mean) / (4 * U_stddev**2)
 
         self.integral_length_scale = hatx
         self.ef100 = ef
@@ -54,13 +47,59 @@ class frequency_analysis():
         self.length = L
 
 
-freq_analysis = frequency_analysis()
-freq_analysis.set_data(data)
-freq_analysis.get_integral_length_scale()
+def test_integral_lenght_scale(show_plot=False):
+    # Here we test the integral length scale computation
+    # Expected results:
+    expected_ILS = 0.006431769932128274
+    expected_ef100 = 0.008682610136218636
 
-plt.plot(freq_analysis.frequencies, freq_analysis.energy[2:int((freq_analysis.length / 2 + 1))])
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel("Frequency, (hz)")
-plt.ylabel("E(f), (m2/s")
-plt.title("Frequency Analysis of Velocity Component {} Signal".format(component))
+    # Read csv result file from SimScale
+    component = "UMag_AIJ_Case_C"
+    probe = "65"
+    path = pathlib.Path.cwd() / "{}.csv".format(component)
+    data = pd.read_csv(path, index_col="Time (s)")
+
+    data = data["{}".format(probe)]
+
+    # Perfrom frequency analysis
+    freq_analysis = frequency_analysis()
+    freq_analysis.set_data(data)
+    freq_analysis.compute_integral_length_scale()
+
+    # Print results
+    error_ILS = (expected_ILS - freq_analysis.integral_length_scale) / (expected_ILS)
+    error_ef100 = (expected_ef100 - freq_analysis.ef100) / (expected_ef100)
+
+    print("Test Integral Length Scale computation:")
+    print()
+    print("Expected values:")
+    print(f"Ingegral Length Scale: {expected_ILS}")
+    print(f"E(f)100: {expected_ef100}")
+    print()
+    print("Computed Values:")
+    print(f"Ingegral Length Scale: {freq_analysis.integral_length_scale}")
+    print(f"E(f)100: {freq_analysis.ef100}")
+    print()
+    print("Computed Errros:")
+    print(f"Ingegral Length Scale: {error_ILS:%}")
+    print(f"E(f)100: {error_ef100:%}")
+
+    if not show_plot:
+        return
+
+    # Plot the energy spectrum
+    plt.plot(
+        freq_analysis.frequencies,
+        freq_analysis.energy[2 : int((freq_analysis.length / 2 + 1))],
+    )
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.xlabel("Frequency, (hz)")
+    plt.ylabel("E(f), (m2/s")
+    plt.title(f"Frequency Analysis of Velocity Component {component} Signal")
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    test_integral_lenght_scale(show_plot=True)
